8c3d8d6080d1a2ecbc9880f595fa244c58c0be40
dnbragin <dnbragin@outlook.com>
2026-02-03 23:58:30 +0300

    fix(RadioButton): fix selected device icon

--- FineTune/Views/Components/RadioButton.swift
+++ FineTune/Views/Components/RadioButton.swift
@@ -10,7 +10,7 @@ struct RadioButton: View {
 
     var body: some View {
         Button(action: action) {
-            Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
+            Image(systemName: isSelected ? "inset.filled.circle" : "circle")
                 .font(.system(size: 14))
                 .symbolRenderingMode(isSelected ? .monochrome : .hierarchical)
                 .foregroundStyle(isSelected ? DesignTokens.Colors.defaultDevice : buttonColor)
